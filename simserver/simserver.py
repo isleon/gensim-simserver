470a27a522e6ccdfc9624a6e221c444092beb5fa
neo <niehongliang@shensz.cn>
2016-06-30 20:08:23 +0800

    doc: update gensim-server.png

--- simserver/simserver.py
+++ simserver/simserver.py
@@ -37,9 +37,9 @@ from simindex import SimIndex, merge_sims
 logger = logging.getLogger('gensim.similarities.simserver')
 
 
-TOP_SIMS = 100 # when precomputing similarities, only consider this many "most similar" documents
-SHARD_SIZE = 65536 # spill index shards to disk in SHARD_SIZE-ed chunks of documents
-DEFAULT_NUM_TOPICS = 400 # use this many topics for topic models unless user specified a value
+# TOP_SIMS = 100 # when precomputing similarities, only consider this many "most similar" documents
+# SHARD_SIZE = 65536 # spill index shards to disk in SHARD_SIZE-ed chunks of documents
+# DEFAULT_NUM_TOPICS = 400 # use this many topics for topic models unless user specified a value
 JOURNAL_MODE = 'OFF' # don't keep journals in sqlite dbs
 
 
